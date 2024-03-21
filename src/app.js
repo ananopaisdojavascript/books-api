87811ee4f41e44c422fe78e3e20885d4b6d42bc4
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-03-21 18:13:43 -0300

    construção do servidor

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import winston from "winston"
+
+const app = express()
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level} - ${message}`
+});
+
+global.logger = winston.createLogger({
+  level: "silly",
+  transports: [
+    new (winston.transports.Console)(), new (winston.transports.File)({ filename: "books.log" })
+  ],
+  format: combine(
+    label({ label: "books" }),
+    timestamp(),
+    myFormat
+  )
+});
+
+app.use((error, request, response, _next) => {
+  logger.error(`${request.method} ${request.baseUrl} - ${error.message}`);
+  response.status(400).send({ error: error.message })
+});
+
+export default app
